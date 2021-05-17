a48b1f32226eb6e41e2e726d1509814127a56af3
DamirlanAmer <>
2021-05-17 12:46:35 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+let player = document.getElementById('player');
+let obstacle = document.getElementById('obstacle');
+let score = document.getElementById('score');
+
+function jump() {
+  player.classList.add('jump-animation');
+  setTimeout(() => {
+    player.classList.remove('jump-animation');
+  }, 700);
+}
+
+document.addEventListener('keypress', () => {
+  if (!player.classList.contains('jump-animation')) {
+    jump();
+  }
+});
+
+setInterval(() => {
+  score.innerText++;
+  let playerTop = parseInt(window.getComputedStyle(player)
+    .getPropertyValue('top'));
+  let obstacleLeft = parseInt(window.getComputedStyle(obstacle)
+    .getPropertyValue('left'));
+  if (obstacleLeft < 0) {
+    obstacle.style.display = 'none';
+  } else {
+    obstacle.style.display = '';
+  }
+
+  if (obstacleLeft < 70 && obstacleLeft > 0 && playerTop > 145) {
+    alert('Game Over! You got a score of: ' + score.innerText +  
+     '\n\nDo you want to play again?');
+    location.reload();
+  }   
+}, 50);
